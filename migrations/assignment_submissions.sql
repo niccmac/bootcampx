1da024ecbdb45af24a3ef39814e47034e0bcd8ce
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-04 16:10:30 -0700

    Add assignments and submissions data

---
+++ migrations/assignment_submissions.sql
@@ -0,0 +1,15 @@
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignments_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
