5cfef758c396af3b8173c69f91d0de61eaf463e3
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-04 15:18:08 -0700

    Update query students no gmail no phone

--- 1_queries/04_students_without_gmail_phone.sql
+++ 1_queries/04_students_without_gmail_phone.sql
@@ -1,6 +1,5 @@
 /* Returns tabel of students with no gmail and no phone */
 SELECT name, id, email, cohort_id 
   FROM students 
-  WHERE email 
-  NOT LIKE '#gmail.com' 
+  WHERE email NOT LIKE '%gmail.com' 
   AND  phone IS NULL;
