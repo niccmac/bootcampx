b342333a04cb935cae4e151556e0268f54aefaf1
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-04 19:43:48 -0700

    Add student avg dubmit duration

---
+++ 3_queries_group-by/5_avr_completion_time.sql
@@ -0,0 +1,8 @@
+/* Returns table of students average assesment duration */
+SELECT name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration 
+FROM students
+JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY AVG(assignment_submissions.duration) DESC;
