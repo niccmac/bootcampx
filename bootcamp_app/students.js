64f400f93d5bae0e2dc08bd7681f72579bf6d323
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-06 20:28:02 -0700

    Update student psql query to parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const args = process.argv.slice(2);
-
-pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`).then(res => {
+const limit = process.argv[1] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+pool.query(queryString,values).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
   });
